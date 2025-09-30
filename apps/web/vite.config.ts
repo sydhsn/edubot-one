4cad08d51e3976109cb53b75f7c4956c7406ae92
“sydhsn” <md.saiyad.husain@ymail.com>
2025-09-30 22:31:56 +0530

    perfect both

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,46 +2,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/web',
+export default defineConfig({
+  plugins: [react()],
   server: {
     port: 4200,
-    host: 'localhost',
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api/v1/',
+        target: 'http://localhost:8000',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
       },
     },
   },
   preview: {
     port: 4200,
-    host: 'localhost',
+    host: true,
   },
-  plugins: [react()],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   build: {
-    outDir: './dist',
+    outDir: 'dist',
     emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
   },
   test: {
-    name: '@edubot-one/web',
-    watch: false,
     globals: true,
     environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const,
-    },
   },
-}));
+});
